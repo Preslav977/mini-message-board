e5f8c08d5b60625f277e0a91643b7ed221649900
Preslav977 <p.cvetanow@gmail.com>
2024-03-13 14:17:35 +0200

    Submitting the form works, now left is to figure out how to show the messages

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,23 @@ const messages = [
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  res.render("index", { messages });
 });
 
 router.get("/new", (req, res, next) => [
   res.render("form", { title: "Mini Message board", messages }),
 ]);
+
+router.post("/new", (req, res) => {
+  console.log(req, res);
+
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
 
 module.exports = router;
